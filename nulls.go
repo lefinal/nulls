ed9bca155365cd631073ffe2662777a40a80dfbb
Lennart Altenhof <l.f.altenhof@gmail.com>
2022-04-27 17:39:57 +0200

    doc: add hints regarding sql and json null values

--- nulls.go
+++ nulls.go
@@ -1,8 +1,10 @@
-// Package nulls provides nullable types like the ones from sql package. However,
-// (un)marshalling support is available as well.
+// Package nulls provides nullable types like the ones from sql package.
+// However, (un)marshalling support is available as well. Keep in mind, that
+// NULL-values and "undefined"-values (JS-style) are treated the same.
 package nulls
 
-// isNull checks if the given byte slice represents NULL value.
+// isNull checks if the given byte slice represents NULL-value or "nothing" (in
+// JS: undefined; here: nil).
 func isNull(b []byte) bool {
 	return b == nil || string(b) == "null"
 }
